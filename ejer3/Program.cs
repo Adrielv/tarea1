298dca73bdf8e43e64248478f9d5bb9a51b0fc45
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2019-05-11 22:40:42 -0400

    Add project files.

---
+++ ejer3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ejer3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valor;
+            double grados;
+            double radianes;
+            Console.WriteLine("Digite los grados:");
+            valor = Console.ReadLine();
+            grados = Convert.ToDouble(valor);
+
+            radianes = grados * ( Math.PI /180);
+            Console.WriteLine("{0} Grados = {1} Radianes", grados, radianes);
+            Console.Read();
+        }
+    }
+}
---
+++ ejer5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ejer5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolares;
+            double canteuros = 0.8902;
+            string valor;
+            Console.WriteLine("Digitar Monto de dolares a convertir a Euros:");
+            valor = Console.ReadLine();
+            dolares = Convert.ToDouble(valor);
+            double conversion = dolares * canteuros;
+
+            Console.WriteLine("{0} Dolares = {1} Euros", dolares, conversion);
+        }
+    }
+}
